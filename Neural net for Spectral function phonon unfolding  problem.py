1fcefcdf6f7ad35a52ab633d8e99b000698c982d
HarshTokyo69 <110736130+HCWorld69@users.noreply.github.com>
2023-03-08 00:28:06 +0900

    Update Neural net for Spectral function phonon unfolding  problem.py

--- Neural net for Spectral function phonon unfolding  problem.py
+++ Neural net for Spectral function phonon unfolding  problem.py
@@ -1,67 +1,50 @@
 # Here's my example PyTorch code for training a neural network to predict the spectral function of a phonon unfolding problem:
 # this is just a basic example, and please contact me if you  may need to modify the code to suit your specific problem. In particular, you'll need to define your own data loading and preprocessing functions to convert your data into PyTorch tensors.
+# Here, ng is the total number of reciprocal lattice vectors entering the summation, omega is the renormalized phonon frequencies, e_tilde and e are the phonon eigenvectors computed using the disordered structure and the unit cell, respectively, g is an index for the reciprocal lattice vectors of the unit cell Brillouin zone, G is a reciprocal lattice vector of the distorted structure, and q is the wavevector in the Brillouin zone of the unit cell. Omega and tilde_Omega are the volumes of the supercell and unit cell, respectively.
+#The forward method of the PhononUnfolding class computes the momentum-resolved spectral function $A(q, \omega)$ using the equations given in the prompt. The PhononUnfolding class can be used to define a PyTorch model for phonon unfolding and trained using any suitable optimizer and loss function.
+
+
+
 
 import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader, TensorDataset
+import torch.nn as nn
 
-# Define the neural network architecture
-class PhononUnfoldingNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(PhononUnfoldingNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+class PhononUnfolding(nn.Module):
+    def __init__(self, ng, omega, e_tilde, e, g, G):
+        super(PhononUnfolding, self).__init__()
+        self.ng = ng
+        self.omega = omega
+        self.e_tilde = e_tilde
+        self.e = e
+        self.g = g
+        self.G = G
 
-    def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
+    def forward(self, q):
+        """
+        :param q: wavevector in the Brillouin zone of the unit cell
+        :return: momentum-resolved spectral function A(q, omega)
+        """
+        P = torch.zeros(len(q), len(self.omega))
+        for i, Q in enumerate(self.Q):
+            for mu in range(len(self.omega)):
+                # calculate spectral weight
+                weight = 0
+                for j, g in enumerate(self.g):
+                    for alpha in range(3):
+                        for kappa in range(len(self.e_tilde)):
+                            unfolded_Q = q[i] + g - self.G
+                            if torch.allclose(Q, unfolded_Q):
+                                weight += torch.abs(torch.dot(self.e_tilde[alpha, kappa, mu, i], 
+                                                               self.e[alpha, kappa, :, g, i])) ** 2
+                weight /= self.ng
+                weight *= self.Omega / self.tilde_Omega
+                P[i, mu] = weight
 
-# Define the hyperparameters
-input_size = 100
-hidden_size = 50
-output_size = 10
-learning_rate = 0.01
-batch_size = 32
-num_epochs = 100
+        # calculate spectral function
+        A = torch.zeros(len(q), len(self.omega))
+        for i in range(len(q)):
+            for mu in range(len(self.omega)):
+                omega_mu = self.omega[mu]
+                A[i, mu] = torch.sum(P[:, mu] * torch.eq(self.omega, omega_mu))
+        return A
 
-# Load the data and split into training and validation sets
-x_train = torch.randn(1000, input_size)
-y_train = torch.randn(1000, output_size)
-x_valid = torch.randn(100, input_size)
-y_valid = torch.randn(100, output_size)
-train_dataset = TensorDataset(x_train, y_train)
-valid_dataset = TensorDataset(x_valid, y_valid)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
-
-# Initialize the neural network and optimizer
-net = PhononUnfoldingNet(input_size, hidden_size, output_size)
-optimizer = optim.Adam(net.parameters(), lr=learning_rate)
-
-# Define the loss function
-criterion = nn.MSELoss()
-
-# Train the neural network
-for epoch in range(num_epochs):
-    train_loss = 0.0
-    valid_loss = 0.0
-    net.train()
-    for i, (inputs, targets) in enumerate(train_loader):
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item() * inputs.size(0)
-    net.eval()
-    with torch.no_grad():
-        for i, (inputs, targets) in enumerate(valid_loader):
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-            valid_loss += loss.item() * inputs.size(0)
-    train_loss /= len(train_dataset)
-    valid_loss /= len(valid_dataset)
-    print('Epoch: {:02d}, Train Loss: {:.4f}, Valid Loss: {:.4f}'.format(epoch+1, train_loss, valid_loss))
